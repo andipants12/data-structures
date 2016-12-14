d20c2b6b909c552c6342d209bf16de16f65610c0
andipants12 <andreareneeo@gmail.com>
2016-12-14 15:14:22 -0800

    My intent with this commit is to have: - Completed functional stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,30 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
+  
+    
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[size] = value;
+    size++;
   };
 
   someInstance.pop = function() {
+    if (size > 0) {
+      var last = size - 1;
+      var popped = storage[last];
+      console.log(last);
+      delete storage[last];
+      size--;
+//      console.log(popped);
+      return popped;
+    }
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
