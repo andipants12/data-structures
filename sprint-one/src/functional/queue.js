d4b5ba25a82b1fec2833c1ce7af854327b691d85
andipants12 <andreareneeo@gmail.com>
2016-12-14 15:30:46 -0800

    With this commit I intend to: -Complete functional queue.js

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,25 @@
 var Queue = function() {
   var someInstance = {};
-
+  var size = 0;
   // Use an object with numeric keys to store values
-  var storage = {};
+  var storage = [];
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage.push(value);
+    size++;
   };
 
   someInstance.dequeue = function() {
+    if (size > 0) {
+      size--;
+      return storage.shift();
+    }
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
