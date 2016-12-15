6f79d1289fce91b2c9774310dc959e4be04d7bcc
andipants12 <andreareneeo@gmail.com>
2016-12-15 15:49:37 -0800

    This commit is all of tests passing for pseudoclassical and prototypal classes passing.

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.sizeI = 0;
+  this.storage = {};
 };
 
 
+Stack.prototype.size = function () {
+  return this.sizeI;
+};
+
+
+Stack.prototype.push = function (value) {
+  this.storage[this.sizeI] = value;
+  this.sizeI++;
+};
+
+Stack.prototype.pop = function () {
+  if(this.sizeI > 0) {
+    this.sizeI--;
+    var popped = this.storage[this.sizeI];
+    delete this.storage[this.sizeI];
+  }
+  return popped;
+}
