1f69a65f1c626a589b50065dbcb1c30fdacea9e4
andipants12 <andreareneeo@gmail.com>
2016-12-15 20:02:22 -0800

    Completed: Linked-list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,13 +3,43 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+
   list.addToTail = function(value) {
+    //this function will add a node with a specific value and reference to the list object
+    //it will also update the reference of the tail and the object preceeding it
+    if (list.tail === null) {
+      list.tail = new Node(value);
+      list.head = list.tail;
+    } else {
+      list.tail = new Node(value);
+      list.head.next = list.tail;
+    }
+    
   };
 
   list.removeHead = function() {
+    var removed = list.head.value;
+    delete list.head;
+    list.head = list.tail;
+    return removed;
   };
 
   list.contains = function(target) {
+    var found = false;
+    var nextNode = list.head;
+    console.log(nextNode);
+    var find = function(node) {
+      if (nextNode.value === target) {
+        found = true;
+      }
+      else if (nextNode.next) {
+        nextNode = nextNode.next;
+        console.log(nextNode);
+        return find(nextNode);
+      }
+    };
+    find(nextNode);
+    return found;
   };
 
   return list;
